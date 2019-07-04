38ddc3bd9a68522cb76327f529a97d2f744fdc0d
IMayanLP <mayan20119@gmail.com>
2019-07-04 15:52:03 -0300

    Finalizando utilização padrão de ReactJS e finalizando app padrao

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
+
 import './style.css'
 
 export default class Main extends Component {
@@ -50,7 +52,8 @@ export default class Main extends Component {
                     <article key={product._id}>
                         <strong>{product.title}</strong>
                         <p>{product.description}</p>
-                        <a href="">Detalhes</a>
+
+                        <Link to={`/products/${product._id}`}>Detalhes</ Link>
                     </article>
                 ))}
                 <div className="actions">
